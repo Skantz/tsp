e25cafe2468d3be43f1b406be63ce6670d8a6e07
Skantz <18464487+Skantz@users.noreply.github.com>
2020-07-29 00:00:00 +0000

    +

--- mst/mst.ml
+++ mst/mst.ml
@@ -1,5 +1,16 @@
 #load "str.cma";;
 
+let query_node_local_cycle n max_n node_lookup = 
+  let () = assert (n > 2) in
+  let x = (Hashtbl.find_opt node_lookup n) in
+  match x with
+  | None -> let () = Printf.printf "%d\n" n in
+    let line = [n - 1 mod max_n; 1; n + 1 mod max_n; 1] in
+    let () = Hashtbl.add node_lookup n (Some line) in
+    line
+  | Some x -> Option.get x
+;;
+
 let query_node n node_lookup =
   (*let n : int = Random.int max_n in*)
   let x = (Hashtbl.find_opt node_lookup n) in
@@ -7,6 +18,7 @@ let query_node n node_lookup =
   | None -> let () = Printf.printf "%d\n" n in
     let line = read_line () in
     let line = Str.split (Str.regexp_string " ") line |> List.map int_of_string in
+    let line = List.tl line in
     let () = Hashtbl.add node_lookup n (Some line) in
     line
   | Some x -> Option.get x
@@ -15,12 +27,12 @@ let query_node n node_lookup =
 let bfs start_node max_n node_lookup =
   let q = Queue.create () in
   let bound = int_of_float (1.0 /. Random.float 1.0) in
-  let h = Hashtbl.create bound in
   let () = Queue.add start_node q in
   let i = ref 0 in
   while !i < bound do 
     let n = Queue.take q in
-    let neighbors = query_node n node_lookup in
+    (*let neighbors = query_node n node_lookup in*)
+    let neighbors = query_node_local_cycle n max_n node_lookup in
     List.iter (fun x -> Queue.add x q) neighbors;
     i := (!i + 1);
   done;
@@ -58,5 +70,6 @@ let max_w = read_int ()
 
 let node_lookup = Hashtbl.create 1000
 
-print_random_number 15
-let main () = query_node 10 node_lookup ()
+(*print_random_number 15
+let main () = query_node 10 node_lookup ()*)
+let main = est_mst n eps max_w node_lookup
