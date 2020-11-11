93856fdd4c41db483564be1c91221bd17e87d47f
rlindsberg <roderick@karlemstrand.com>
2020-11-11 09:20:25 +0100

    beta implementation of visulisation

--- mst/mst_algorithm_visulise.py
+++ mst/mst_algorithm_visulise.py
@@ -60,6 +60,7 @@ class MST:
         """
         Approximate the number of components for each of the weights
         """
+        print("approx_conponents is called\n")
 
         G = self.graph
 
@@ -68,23 +69,22 @@ class MST:
         # plt.savefig("ba.png")
         # plt.show()
 
-        color_lookup = {k: 0 for v, k in enumerate(sorted(set(G.graph.nodes())))}
-        # {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'J': 6, 'K': 7, 'Z': 8}
+
 
         # nx.draw_networkx(G)
         # plt.show()
-        low, *_, high = sorted(color_lookup.values())
-        norm = mpl.colors.Normalize(vmin=low, vmax=high, clip=True)
-        mapper = mpl.cm.ScalarMappable(norm=norm, cmap=mpl.cm.coolwarm)
-
-        rcParams['figure.figsize'] = 12, 7
-        nx.draw(G.graph,
-                nodelist=color_lookup,
-                node_size=1000,
-                node_color=[mapper.to_rgba(i)
-                            for i in color_lookup.values()],
-                with_labels=True)
-        plt.show()
+        # low, *_, high = sorted(color_lookup.values())
+        # norm = mpl.colors.Normalize(vmin=low, vmax=high, clip=True)
+        # mapper = mpl.cm.ScalarMappable(norm=norm, cmap=mpl.cm.coolwarm)
+        #
+        # rcParams['figure.figsize'] = 12, 7
+        # nx.draw(G.graph,
+        #         nodelist=color_lookup,
+        #         node_size=1000,
+        #         node_color=[mapper.to_rgba(i)
+        #                     for i in color_lookup.values()],
+        #         with_labels=True)
+        # plt.show()
 
 
         i = 0
@@ -96,8 +96,12 @@ class MST:
         chosen_us = set()
 
         # Sample nodes until we hit the node limit
-        while G.curr_read < G.max_read and time.time() - self.start_time < self.time_limit:
+        while G.curr_read < G.max_read and i < 25:
             i += 1
+            print('Within time limit:')
+            print(self.time_limit)
+            print('i is:')
+            print(i)
 
             while True:
                 start = random.randint(0, G.size - 1)
@@ -110,37 +114,107 @@ class MST:
             X = int(1.0 / (1.0 - random.random()))
 
             for weight in range(G.max_weight, 0, -1):
+
+                ##### search for new weight, reset
+                color_lookup = {k: 0 for v, k in enumerate(sorted(set(G.graph.nodes())))}
+                labels_lookup = {}
+                # {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'J': 6, 'K': 7, 'Z': 8}
+                bfs_counter = 0
+                #####
+
+                print('############################# Searching weight ' + str(weight) + ' #############################')
+                print('bfs counter is')
+                print(bfs_counter)
+
+
                 nodes_queue = Queue()
                 nodes_queue.put(start)
                 queued_nodes = {start}
 
-                while G.curr_read < G.max_read and time.time() - self.start_time < self.time_limit:
+                while G.curr_read < G.max_read and i < 25:
+                    bfs_counter += 1
+
+                    print('############################# Reading ' + str(G.curr_read) + ' #############################')
+                    print('bfs counter is')
+                    print(bfs_counter)
+
                     node = nodes_queue.get()
                     color_lookup[node] = 10
 
-                    ####
+                    print('color lookup is')
+                    print(color_lookup)
+
+                    #### setting labels
+                    labels_lookup[node] = str(bfs_counter)
+                    #### end setting labels
 
                     low, *_, high = sorted(color_lookup.values())
                     norm = mpl.colors.Normalize(vmin=low, vmax=high, clip=True)
                     mapper = mpl.cm.ScalarMappable(norm=norm, cmap=mpl.cm.coolwarm)
 
-                    rcParams['figure.figsize'] = 12, 7
-                    nx.draw(G.graph,
-                            nodelist=color_lookup,
-                            node_size=1000,
-                            node_color=[mapper.to_rgba(i)
-                                        for i in color_lookup.values()],
-                            with_labels=True)
+
+                    # pos = nx.get_node_attributes(G.graph, 'pos')
+                    # pos = nx.spring_layout(G.graph)  # positions for all nodes
+                    pos = dict()
+                    pos[0] = [0, 0]
+                    pos[1] = [2, 2]
+                    pos[2] = [2, 0]
+                    pos[3] = [0, 2]
+                    pos[4] = [1, 1]
+
+
+                    #### draw
+                    node_colour = [mapper.to_rgba(i) for i in color_lookup.values()]
+                    labels = nx.get_edge_attributes(G.graph, 'weight')
+                    nx.draw_networkx_nodes(G.graph, pos, node_size=200, node_color=node_colour)
+                    nx.draw_networkx_edges(G.graph, pos, edgelist=G.graph.edges(data=True))
+                    nx.draw_networkx_labels(G.graph, pos, labels_lookup, font_size=20, font_color='white')
+                    nx.draw_networkx_edge_labels(G.graph, pos, edge_labels=labels)
                     plt.show()
-
-
-                    ######ß
+                    ######
 
 
                     for neighbor in G.query(node):
                         if neighbor[0] not in queued_nodes and neighbor[1] <= weight:
+
+
+                            node = neighbor[0]
+                            print('Sampling node' + str(node))
+
+                            bfs_counter += 1
+                            color_lookup[node] = 10
+                            labels_lookup[node] = str(bfs_counter)
+
+                            print('color lookup is')
+                            print(color_lookup)
+
+                            #### draw
+                            node_colour = [mapper.to_rgba(i) for i in color_lookup.values()]
+                            labels = nx.get_edge_attributes(G.graph, 'weight')
+                            nx.draw_networkx_nodes(G.graph, pos, node_size=200, node_color=node_colour)
+                            nx.draw_networkx_edges(G.graph, pos, edgelist=G.graph.edges(data=True))
+                            nx.draw_networkx_labels(G.graph, pos, labels_lookup, font_size=20, font_color='white')
+                            nx.draw_networkx_edge_labels(G.graph, pos, edge_labels=labels)
+                            plt.show()
+                            ######
+
+
                             nodes_queue.put(neighbor[0])
                             queued_nodes.add(neighbor[0])
+
+                            # color_lookup[neighbor[0]] = 10
+                            # low, *_, high = sorted(color_lookup.values())
+                            # norm = mpl.colors.Normalize(vmin=low, vmax=high, clip=True)
+                            # mapper = mpl.cm.ScalarMappable(norm=norm, cmap=mpl.cm.coolwarm)
+                            #
+                            # nx.draw(G.graph,
+                            #         nodelist=color_lookup,
+                            #         node_size=100,
+                            #         node_color=[mapper.to_rgba(i)
+                            #                     for i in color_lookup.values()],
+                            #         with_labels=True)
+                            # plt.show()
+
 
                     if len(queued_nodes) > X:
                         break
@@ -148,7 +222,9 @@ class MST:
                     if nodes_queue.empty():
                         betas[weight - 1] += 1
                         break
-
+                print('############################# Finished BFS ############################# \n')
+        print('time up')
+        print(time.time() - self.start_time)
         if i <= 1:
             return [0 for _ in range(G.max_weight)]
 
